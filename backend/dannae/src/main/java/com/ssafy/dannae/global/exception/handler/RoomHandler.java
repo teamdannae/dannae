b37de970d356efa37331062b5b358fc155126194
kbs <bumsoo0703@naver.com>
2024-10-30 14:55:43 +0900

    fix: import안해온 부분 수정

--- backend/dannae/src/main/java/com/ssafy/dannae/global/exception/handler/RoomHandler.java
+++ backend/dannae/src/main/java/com/ssafy/dannae/global/exception/handler/RoomHandler.java
@@ -1,5 +1,7 @@
 package com.ssafy.dannae.global.exception.handler;
 
+import static com.ssafy.dannae.global.exception.ResponseCode.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
