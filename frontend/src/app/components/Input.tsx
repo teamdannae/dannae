d28ce189bdd0c048974b743fd2ccc2f3c9f6beb6
정상영 <jeongsangyoung@naver.com>
2024-11-01 14:30:54 +0900

    feat: input 공통 컴퍼넌트 구현

--- frontend/src/app/components/Input.tsx
+++ frontend/src/app/components/Input.tsx
@@ -1,15 +1,38 @@
-const Input = () => {
-  /* props
-        1. type
-        2. placeholder
-        3. value
-        4. onChange
-        5. disabled
-        6. pattern
-    */
+import Image from 'next/image';
+import styles from './common.module.scss';
+
+interface InputProps {
+  value: string;
+  onChangeEvent: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputLabel?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  isValid?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  value,
+  onChangeEvent,
+  inputLabel,
+  placeholder = "",
+  disabled = false,
+  isValid = true,
+}: InputProps) => {
   return (
-    <div>
-      <h2>Input</h2>
+    <div className={styles.inputFormContainer}>
+      <label className={styles.inputLabel}>{inputLabel}</label>
+      <div className={styles.inputWrapper}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChangeEvent}
+          placeholder={placeholder}
+          disabled={disabled}
+          className={`${styles.inputValue} ${isValid ? "" : styles.invalidInputValue}`}
+        />
+        {/* 유효성 검사 실패시 아이콘 보여주기 */}
+        {!isValid && <Image src="/icons/warning.svg" alt="warning" width={48} height={48} className={styles.warningIcon} />}
+      </div>
     </div>
   );
 };
