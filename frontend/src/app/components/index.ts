482321d2c9ab1993fbdac6462627eca775357ee0
iltae94 <iltae94@gmail.com>
2024-11-01 16:39:02 +0900

    feat: 파일 중앙 집중화 관리

--- frontend/src/app/components/index.ts
+++ frontend/src/app/components/index.ts
@@ -1,1 +1,6 @@
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Input } from "./Input";
 export { default as Progress } from "./Progress";
+export { default as Radio } from "./Radio";
+export { default as Word } from "./Word";
