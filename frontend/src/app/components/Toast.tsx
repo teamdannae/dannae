9aeb3f67891e61b765206e1523b5babbd52e8036
iltae94 <iltae94@gmail.com>
2024-11-04 15:24:48 +0900

    feat: 공통 컴퍼넌트 toast 개발

---
+++ frontend/src/app/components/Toast.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./common.module.scss";
+
+interface ToastProps {
+  message: string;
+  duration?: number;
+}
+
+export default function Toast({ message, duration = 1000 }: ToastProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+    const timer = setTimeout(() => setIsVisible(false), duration);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  if (!isVisible) return null;
+
+  return (
+    <div className={`${styles.toast} ${isVisible ? styles.visible : ""}`}>
+      <h4>{message}</h4>
+    </div>
+  );
+}
