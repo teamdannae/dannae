b72dd6fee914dfd2a9a0488ba44178e9e8a0ba2c
kbs <bumsoo0703@naver.com>
2024-11-04 12:40:18 +0900

    fix: query, command 변경 및 get을 read로 변경

--- backend/dannae/src/main/java/com/ssafy/dannae/domain/room/service/RoomQueryService.java
+++ backend/dannae/src/main/java/com/ssafy/dannae/domain/room/service/RoomQueryService.java
@@ -1,11 +1,11 @@
 package com.ssafy.dannae.domain.room.service;
 
+import java.util.List;
+
 import com.ssafy.dannae.domain.room.service.dto.RoomDto;
 
 public interface RoomQueryService {
 
-	RoomDto createRoom(RoomDto roomDto);
-
-	void updateRoom(Long roomId, RoomDto roomDto);
+	List<RoomDto> readReleasedRooms();
 
 }
