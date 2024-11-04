7f7d8832886197173bd419bbd608e7d5b1aa615e
iltae94 <iltae94@gmail.com>
2024-11-04 15:25:28 +0900

    feat: 게임 대기방 헤더 개발

--- frontend/src/app/waiting-room/components/Header.tsx
+++ frontend/src/app/waiting-room/components/Header.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
-import styles from "./components.module.css";
+import { BackButton, CopyCode } from "../components";
+import styles from "./components.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <button>
-        <span className={styles.backIcon} />
-        나가기
-      </button>
+      <BackButton />
       <h2 className={styles.roomTitle}>범수랑 놀아주는 방</h2>
-      <button>
-        <Image src="/icons/copy.svg" alt="copy-icon" width={24} height={24} />
-      </button>
+      <CopyCode />
     </header>
   );
 }
--- frontend/src/app/waiting-room/components/index.ts
+++ frontend/src/app/waiting-room/components/index.ts
@@ -2,3 +2,5 @@ export { default as Header } from "./Header";
 export { default as GameInfo } from "./GameInfo";
 export { default as PlayerList } from "./PlayerList";
 export { default as Chat } from "./Chat";
+export { default as CopyCode } from "./CopyCode";
+export { default as BackButton } from "./BackButton";
