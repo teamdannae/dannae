fb55b80f96b12783b8b44a463c520ed1cd4d6f4e
정상영 <jeongsangyoung@naver.com>
2024-11-05 15:04:16 +0900

    feat: 쿠키에 닉네임, 이미지 없으면 설정 화면으로 인터셉터 처리하도록 미들웨어 구현

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,25 @@
+// src/middleware.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // 루트 URL을 예외 처리하여 리디렉션이 발생하지 않도록 설정
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.next();
+  }
+
+  // 쿠키에서 nickname과 image 값을 가져옴
+  const nickname = request.cookies.get('nickname')?.value;
+  const image = request.cookies.get('image')?.value;
+
+  // nickname이나 image가 없으면 리다이렉트
+  if (!nickname || !image) {
+    return NextResponse.redirect(new URL('/profile/nickname', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// 특정 경로 또는 모든 경로에 미들웨어를 적용
+export const config = {
+  matcher: ['/((?!api|_next|favicon.ico|.*\\.svg$|profile/nickname|profile/image).*)'],
+};
