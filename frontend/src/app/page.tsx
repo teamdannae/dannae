e99c283ccaf09265e0cea71d348e3354f9a63c3a
정상영 <jeongsangyoung@naver.com>
2024-11-05 15:04:26 +0900

    feat: 대문 페이지 임시 구현

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Progress } from "./components";
+"use client"
+
+import { useRouter } from "next/navigation";
+import { Button } from "./components";
 
 export default function Home() {
+  const router = useRouter();
+
+  const navigateToGame = () => {
+    router.push("/lobby");
+  }
+
   return (
     <div>
-      <Progress duration={10} />
+      <h1>대문 공사중</h1>
+      <Button buttonText="게임하기" onClickEvent={navigateToGame} buttonColor="black"/>
     </div>
   );
 }
