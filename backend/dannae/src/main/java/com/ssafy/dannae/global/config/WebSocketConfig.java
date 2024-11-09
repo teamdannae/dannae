513786f4e311247f5ac4aa9ee8dfd4650b635dd6
hwang <hyb26@naver.com>
2024-11-10 03:42:20 +0900

    feat: 무한 초성 지옥 웹소켓 구현

--- backend/dannae/src/main/java/com/ssafy/dannae/global/config/WebSocketConfig.java
+++ backend/dannae/src/main/java/com/ssafy/dannae/global/config/WebSocketConfig.java
@@ -1,12 +1,14 @@
 package com.ssafy.dannae.global.config;
 
-import com.ssafy.dannae.global.exception.handler.SentenceGameWebSocketHandler;
-import com.ssafy.dannae.global.exception.handler.WaitingRoomWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import com.ssafy.dannae.global.exception.handler.InfiniteGameWebSocketHandler;
+import com.ssafy.dannae.global.exception.handler.SentenceGameWebSocketHandler;
+import com.ssafy.dannae.global.exception.handler.WaitingRoomWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
@@ -14,11 +16,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final WaitingRoomWebSocketHandler waitingRoomWebSocketHandler;
     private final SentenceGameWebSocketHandler sentenceGameWebSocketHandler;
+    private final InfiniteGameWebSocketHandler infiniteGameWebSocketHandler;
 
-    public WebSocketConfig( WaitingRoomWebSocketHandler waitingRoomWebSocketHandler,  SentenceGameWebSocketHandler sentenceGameWebSocketHandler) {
+    public WebSocketConfig( WaitingRoomWebSocketHandler waitingRoomWebSocketHandler,  SentenceGameWebSocketHandler sentenceGameWebSocketHandler,
+		InfiniteGameWebSocketHandler infiniteGameWebSocketHandler) {
         this.waitingRoomWebSocketHandler = waitingRoomWebSocketHandler;
         this.sentenceGameWebSocketHandler = sentenceGameWebSocketHandler;
-    }
+		this.infiniteGameWebSocketHandler = infiniteGameWebSocketHandler;
+	}
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -30,5 +35,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(sentenceGameWebSocketHandler, "/ws/sentencegame")
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .setAllowedOriginPatterns("*");
+
+        registry.addHandler(infiniteGameWebSocketHandler, "/ws/infinitegame")
+            .addInterceptors(new HttpSessionHandshakeInterceptor())
+            .setAllowedOriginPatterns("*");
     }
 }
