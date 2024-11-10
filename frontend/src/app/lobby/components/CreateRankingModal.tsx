883daab1e67a3ec5abb7fc4286ca69d5e30949e6
iltae94 <iltae94@gmail.com>
2024-11-10 17:10:53 +0900

    feat: 랭킹 조회 api 기능 연결

--- frontend/src/app/lobby/components/CreateRankingModal.tsx
+++ frontend/src/app/lobby/components/CreateRankingModal.tsx
@@ -2,9 +2,29 @@
 import styles from "./component.module.scss";
 // import { useModal } from "@/hooks";
 // import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const CreateRankingModal = () => {
   // const router = useRouter();
+
+  const loadRanking = async () => {
+    try {
+      const response = await fetch("/api/next/game/ranking");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch games");
+      }
+
+      const rankingData = await response.json();
+      console.log(rankingData);
+    } catch (error) {
+      console.error("Failed to load games:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadRanking();
+  }, []);
 
   return (
     <div className={styles.modalContainer}>
