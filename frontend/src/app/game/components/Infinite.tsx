4d7e0756387c26b50812f99b98e4051d4fe798d6
정상영 <jeongsangyoung@naver.com>
2024-11-11 09:51:24 +0900

    feat: 무한 초성 지옥 게임 화면 구현

--- frontend/src/app/game/components/Infinite.tsx
+++ frontend/src/app/game/components/Infinite.tsx
@@ -1,12 +1,34 @@
+import styles from "./components.module.scss";
+
 interface InfiniteProps {
-  wordList: string[];
+  wordList: InfiniteWord[];
+  consonants: string;
 }
 
-const Infinite = ({ wordList }: InfiniteProps) => {
+const Infinite = ({ wordList, consonants }: InfiniteProps) => {
   return (
-    <div>
-      <h1>무한 초성 게임</h1>
-      <h3>{wordList}</h3>
+    <div className={styles.infiniteGameContainer}>
+      <div className={styles.consonantContainer}>
+        {Array.from(consonants).map((consonant, index) => (
+          <div key={index} className={styles.consonant}>
+            <h1>{consonant}</h1>
+          </div>
+        ))}
+      </div>
+      <div className={styles.wordContainer}>
+        {wordList.map((word) => (
+          <div
+            key={word.word}
+            className={`${styles.word} ${
+              word.correct
+                ? styles[`difficulty-${word.difficulty}`]
+                : styles.fail
+            }`}
+          >
+            <h5>{word.word}</h5>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
