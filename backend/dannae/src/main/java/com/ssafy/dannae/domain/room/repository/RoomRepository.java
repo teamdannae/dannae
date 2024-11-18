80fb7abdc35aa789dafa46359323c3c926ec4be6
hwang <hyb26@naver.com>
2024-11-18 13:26:20 +0900

    feat: 게임중인 방도 올라가게 함

--- backend/dannae/src/main/java/com/ssafy/dannae/domain/room/repository/RoomRepository.java
+++ backend/dannae/src/main/java/com/ssafy/dannae/domain/room/repository/RoomRepository.java
@@ -1,17 +1,14 @@
 package com.ssafy.dannae.domain.room.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.dannae.domain.room.entity.Room;
-import com.ssafy.dannae.domain.room.entity.RoomStatus;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-
-	List<Room> findByReleaseAndStatusAndPlayerCountGreaterThanOrderByIdDesc(Boolean release, RoomStatus status, int playerCount);
+	List<Room> findByReleaseAndPlayerCountGreaterThanOrderByIdDesc(boolean release, int playerCount);
 
 	boolean existsById(Long id);
 
--- backend/dannae/src/main/java/com/ssafy/dannae/domain/room/service/Impl/RoomQueryServiceImpl.java
+++ backend/dannae/src/main/java/com/ssafy/dannae/domain/room/service/Impl/RoomQueryServiceImpl.java
@@ -32,7 +32,7 @@ class RoomQueryServiceImpl implements RoomQueryService {
 	@Override
 	public List<RoomDto> readReleasedRooms() {
 
-		List<Room> roomList = roomRepository.findByReleaseAndStatusAndPlayerCountGreaterThanOrderByIdDesc(true, RoomStatus.READY, 0);
+		List<Room> roomList = roomRepository.findByReleaseAndPlayerCountGreaterThanOrderByIdDesc(true, 0);
 
 		return roomList.stream()
 			.map(room -> RoomDto.builder()
@@ -41,6 +41,7 @@ class RoomQueryServiceImpl implements RoomQueryService {
 				.mode(room.getMode())
 				.playerCount(room.getPlayerCount())
 				.creator(room.getCreator())
+				.status(room.getStatus())
 				.creatorNickname(
 					playerRepository.findById(room.getCreator())
 						.orElseThrow(() -> new NoPlayerException("없는 사용자입니다."))
